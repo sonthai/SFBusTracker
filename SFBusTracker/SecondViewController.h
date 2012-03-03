6d55f00865532054facb7e3be77a6b483f726f62
Son Thai <sonthai@Sons-MacBook-Pro.local>
2012-03-02 22:21:14 -0800

    Initial Commit

---
+++ SFBusTracker/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  SFBusTracker
+//
+//  Created by Son Thai on 3/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
